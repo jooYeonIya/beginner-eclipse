2d86a5ee0b2d946f5b3f2f270bce38eeac1bd859
jooYeonIya <jooyeonstudyios@gmail.com>
2024-08-19 23:46:22 +0900

    [ch02.sec04] 실수 타입

---
+++ beginnerJava/src/ch02/sec04/FloatDeoubleDemo.java
@@ -0,0 +1,13 @@
+package ch02.sec04;
+
+public class FloatDeoubleDemo {
+
+	public static void main(String[] args) {
+		float var1 = 0.12345678901234567890f;
+		double var2 = 0.12345678901234567890;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+	}
+
+}
