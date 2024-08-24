8de365c48dbeca42cea5ae822c8de787ffb6118c
jooYeonIya <jooyeonstudyios@gmail.com>
2024-08-25 04:57:03 +0900

    [ch04.sec04] for문

---
+++ beginnerJava/src/ch04/sec04/ForDemo.java
@@ -0,0 +1,16 @@
+package ch04.sec04;
+
+import java.util.Iterator;
+
+public class ForDemo {
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for (int i = 1; i <= 10; i++) {
+			sum += i;
+		}
+		
+		System.out.println(sum);
+	}
+	
+}
