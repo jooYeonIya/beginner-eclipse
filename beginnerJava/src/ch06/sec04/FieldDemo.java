952fb38add2e2efe07ae9d6b827bc6e5509fc9d1
jooYeonIya <jooyeonstudyios@gmail.com>
2024-08-25 22:58:22 +0900

    [ch06.sec04] 객체 생성

---
+++ beginnerJava/src/ch06/sec04/FieldDemo.java
@@ -0,0 +1,17 @@
+package ch06.sec04;
+
+// 실행 클래스
+public class FieldDemo {
+	public static void main(String[] args) {
+		// 객체 생성
+		// 클래스 변수 = new 클래스(); 
+		Car car1 = new Car();
+	}
+}
+
+// 라이브러리 클래
+class Car {
+	String model;
+	boolean start;
+	int speed;
+}
