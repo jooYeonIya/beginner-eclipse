fb4de02a9bdef154f2e448b64606cc9a2d2d3e8b
jooyeoniya <jooyeonstudyios@gmail.com>
2024-08-28 14:07:53 +0900

    [ch06.sec05] 클래스 구성 멤버

---
+++ beginnerJava/src/ch06/sec05/ClassName.java
@@ -0,0 +1,16 @@
+package ch06.sec05;
+
+public class ClassName {
+
+  // 필드 선언
+  int fieldName;
+
+  // 생성자 선언
+  public ClassName() {
+  }
+
+  // 메소드 선언
+  int methodName() {
+    return 0;
+  }
+}
