1a98c0ebb808b338a9d95ffe8c9362fb1c58a02a
jooYeonIya <jooyeonstudyios@gmail.com>
2024-08-31 23:58:04 +0900

    [ch06.sec11] 상수 (Final)

--- beginnerJava/src/ch06/sec11/FinalDemo.java
+++ beginnerJava/src/ch06/sec11/FinalDemo.java
@@ -1,5 +1,23 @@
 package ch06.sec11;
 
 public class FinalDemo {
+	public static void main(String[] args) {
+		Korean korean = new Korean("123456789");
+		
+		// 상수는 값 변경할 수 없
+		// korean.nation = "미국";
+		// korean.ssn = "987456";
+	}
+}
 
+
+class Korean {
+	// 필드 선언 시 초기값 대입
+	final String nation = "한국";
+	final String ssn;
+	
+	//생성자에서 초기값 대입
+	public Korean(String ssn) {
+		this.ssn = ssn;
+	}
 }
