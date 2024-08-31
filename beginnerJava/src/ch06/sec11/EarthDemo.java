c6d221bd463e9569ee6dffd94d700e815433a23e
jooYeonIya <jooyeonstudyios@gmail.com>
2024-08-31 23:58:13 +0900

    [ch06.sec11] 상수

--- beginnerJava/src/ch06/sec11/EarthDemo.java
+++ beginnerJava/src/ch06/sec11/EarthDemo.java
@@ -1,10 +1,21 @@
 package ch06.sec11;
 
 public class EarthDemo {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 상수값 사용
+		System.out.println(Earth.EARTH_RADIUS);
 	}
+}
 
+class Earth {
+	// 상수 선언 및 초기화 
+	static final double EARTH_RADIUS = 6400;
+	
+	// 상수 선언
+	static final double EARTH_SAURFACE_AREA;
+	
+	// 정적 블록에서 상수 초기화
+	static {
+		EARTH_SAURFACE_AREA = 4 * EARTH_RADIUS;
+	}
 }
