a34113c105c3daeb75eb37e00fb1b133dcbb8238
jooYeonIya <jooyeonstudyios@gmail.com>
2024-09-03 02:51:34 +0900

    고객 클래스 생성자 생성

--- beginnerJava/src/Mission/Customer.java
+++ beginnerJava/src/Mission/Customer.java
@@ -9,6 +9,13 @@ public class Customer {
     LocalDate firstTransactionDate; // 신규 거래 일자
     LocalDate lastTransactionDate; 	// 최종 거래 일자
 
+   public Customer(BankCode bankCode, String customerNumber, String name) {
+	   this.bankCode = bankCode;
+	   this.customerNumber = customerNumber;
+	   this.name = name;
+	   this.firstTransactionDate = LocalDate.now();
+	   this.lastTransactionDate = LocalDate.now();	   
+   }
 }
 
 //고객의 cru 는 거래가 있을때 같이 진행됨, 고객정보 삭제는 이번에는 고려대상이 아님
