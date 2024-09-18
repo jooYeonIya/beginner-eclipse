ce09e5f222ffcce3bdf81482ac4c74ebddcdf263
kang jooyeon <jooyeonstudyios@gmail.com>
2024-09-18 13:09:58 +0900

    ch14.sec03 작업 스레드 생성과 실행

---
+++ beginnerJava/src/ch14/BeepPrintDemo2.java
@@ -0,0 +1,25 @@
+package beginnerJava.src.ch14;
+
+import java.awt.*;
+
+public class BeepPrintDemo2 {
+    public static void main(String[] args) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                Toolkit toolkit = Toolkit.getDefaultToolkit();
+                for (int i = 0; i < 5; i++) {
+                    toolkit.beep();
+                    try { Thread.sleep(500); } catch (Exception e) {}
+                }
+            }
+        };
+
+        thread.start();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("띵");
+            try { Thread.sleep(500); } catch (Exception e) {}
+        }
+    }
+}
